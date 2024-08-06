e20153e67dd2e2b057d315e8df9a46771074db4f
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-08-06 14:24:28 +0200

    feat: Add level two and level three buttons to paired-reading page

--- src/routes/section/paired-reading/+page.svelte
+++ src/routes/section/paired-reading/+page.svelte
@@ -3,4 +3,7 @@
 </script>
 
 <ButtonStartLesson href="/lesson/paired-reading/zero">Level Zero</ButtonStartLesson>
+<ButtonStartLesson href="/lesson/paired-reading/zero-voice">Level Zero Voice</ButtonStartLesson>
 <ButtonStartLesson href="/lesson/paired-reading/one">Level One</ButtonStartLesson>
+<ButtonStartLesson href="/lesson/paired-reading/two">Level Two</ButtonStartLesson>
+<ButtonStartLesson href="/lesson/paired-reading/three">Level Three</ButtonStartLesson>
