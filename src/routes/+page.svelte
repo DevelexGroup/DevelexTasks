80a4ab56c47a01445c8f2dd716278b8c17dff99b
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-08-12 15:27:00 +0200

    feat: Add debug button for Paired Reading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,4 @@
 </script>
 
 <Button href="/section/paired-reading">Paired Reading</Button>
+<Button href="/debug-section/paired-reading">Paired Reading (debug)</Button>
--- src/routes/debug-section/+page.svelte
+++ src/routes/debug-section/+page.svelte
@@ -2,5 +2,4 @@
 	import Button from '$lib/components/Button.svelte';
 </script>
 
-<Button href="/section/paired-reading">Paired Reading</Button>
 <Button href="/debug-section/paired-reading">Debug Paired Reading</Button>
