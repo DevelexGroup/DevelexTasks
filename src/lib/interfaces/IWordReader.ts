75c26d45bf4e66da77e60621c0d0b8063fb84acb
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-09-24 15:33:48 +0200

    refactor: Add onWordChange callback to IWordReader interface

--- src/lib/interfaces/IWordReader.ts
+++ src/lib/interfaces/IWordReader.ts
@@ -1,4 +1,12 @@
 export interface IWordReader {
 	read(words: { id: string; text: string }[]): Promise<void>;
 	abort(): void;
+	onWordChange?: (
+		word: {
+			text: string;
+			id: string;
+			start: number;
+			end: number;
+		} | null
+	) => void;
 }
