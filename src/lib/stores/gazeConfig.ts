81ae407b78ed1693a6ec2b3f7ff11b80669f2437
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-11-25 00:50:05 +0100

    Revert "feat: Update gaze input configuration with new tracker and detection settings"
    This reverts commit 55b7418cde52fabb4e3ef906be771bd9e5609dee.

--- src/lib/stores/gazeConfig.ts
+++ src/lib/stores/gazeConfig.ts
@@ -1,19 +1,10 @@
 import { type GazeInputConfigWithFixations } from '@473783/develex-core';
 import { writable, type Writable } from 'svelte/store';
 
-/* export const inputCreationConfig: Writable<GazeInputConfigWithFixations> = writable({
+export const inputCreationConfig: Writable<GazeInputConfigWithFixations> = writable({
 	tracker: 'opengaze',
 	uri: 'ws://localhost:13892',
 	fixationDetection: 'device'
-}); */
-
-export const inputCreationConfig: Writable<GazeInputConfigWithFixations> = writable({
-	tracker: 'dummy',
-	fixationDetection: 'idt',
-	frequency: 60,
-	precisionMinimalError: 10,
-	precisionDecayRate: 0.1,
-	precisionMaximumError: 100
 });
 
 interface ConfigMouseEventFields {
