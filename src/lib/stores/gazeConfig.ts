00dce4ef4e82182f1ed51a1c7ca3155e086b6803
Javierko <33197433+Javierko@users.noreply.github.com>
2025-02-06 19:34:06 +0100

    feat: default mouse tracker

--- src/lib/stores/gazeConfig.ts
+++ src/lib/stores/gazeConfig.ts
@@ -2,7 +2,10 @@ import { type GazeInputConfigWithFixations } from '@473783/develex-core';
 import { writable, type Writable } from 'svelte/store';
 
 export const inputCreationConfig: Writable<GazeInputConfigWithFixations> = writable({
-	tracker: 'gazepoint',
-	uri: 'ws://localhost:13892',
-	fixationDetection: 'device'
+	tracker: 'dummy',
+	fixationDetection: 'idt',
+	frequency: 60,
+	precisionMinimalError: 0.5,
+	precisionDecayRate: 0.1,
+	precisionMaximumError: 1.5
 });
