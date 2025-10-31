dfcdf0d4392a832c324047079497576d41fe148d
Javierko <javierkoo21@gmail.com>
2025-10-31 14:41:04 +0100

    feat: svelte init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Develex Tasks</title>
+	<meta name="description" content="Various develex tasks" />
+</svelte:head>
+
+<section class="mt-8 flex items-center justify-center">
+	<h1 class="text-5xl font-bold text-red-400">Develex Tasks</h1>
+</section>
