ad343eebfc85124f61eaba62fad2eb3389504a05
Michal Petr <jeerdus55@gmail.com>
2025-11-24 10:41:38 +0100

    feat(cibule): Incorrect flash in practice

--- src/lib/components/tasks/cibule/components/CibuleSymbol.svelte
+++ src/lib/components/tasks/cibule/components/CibuleSymbol.svelte
@@ -22,7 +22,13 @@
 
 	function onSymbolClick(): void {
 		let validationResult = validateSymbolClick(symbol, index);
-		isSelected = true;
+
+		if (!isCorrect) {
+			isSelected = false;
+			setTimeout(() => {
+				isSelected = true;
+			}, 10);
+		}
 
 		const audio = new Audio(
 			resolveAny(validationResult ? '/sound/correct.mp3' : '/sound/mistake.mp3')
@@ -47,41 +53,62 @@
 
 <style>
 	.symbol {
-			cursor: pointer;
-			position: relative;
-			user-select: none;
+		cursor: pointer;
+		position: relative;
+		user-select: none;
 
-			&.non-interactable{
-					pointer-events: none;
+		&.non-interactable{
+				pointer-events: none;
+		}
+
+		&.selected{
+			&.correct-symbol {
+				color: #1E40AF;
+				&::before {
+					background-color: #bbc5e7;
+					z-index: -1;
+				}
 			}
 
-			&.selected{
-					&.correct-symbol{
-              color: #1E40AF;
-              &::before {
-                  background-color: #bbc5e7;
-                  z-index: -1;
-              }
-					}
-
-					&.incorrect-symbol{
-							color: #DC2626;
-							&::before {
-									background-color: #f4bdbd;
-									z-index: -2;
-							}
-					}
+			&.incorrect-symbol{
+				--animation-time: 300ms;
+				animation: incorrect-flash-text var(--animation-time) ease-in-out 2;
+				&::before {
+					animation: incorrect-flash-bg var(--animation-time) ease-in-out 2;
+				}
 			}
+		}
 
-			&::before{
-					content: "";
-					position: absolute;
-					inset: -0.375rem;
-					border-radius: 0.5rem;
-					background-color: rgba(0, 0, 0, 0);
-					transition: all 200ms ease-in;
-					pointer-events: none;
-			}
+		&::before{
+			content: "";
+			position: absolute;
+			inset: -0.375rem;
+			border-radius: 0.5rem;
+			background-color: rgba(0, 0, 0, 0);
+			transition: all 200ms ease-in;
+			pointer-events: none;
+		}
+	}
+
+	@keyframes incorrect-flash-text {
+		0%, 100% {
+			color: inherit;
+		}
+
+		50% {
+			color: #DC2626;
+		}
+	}
+
+	@keyframes incorrect-flash-bg {
+		0%, 100% {
+			background-color: rgba(0, 0, 0, 0);
+			z-index: auto;
+		}
+
+		50% {
+			background-color: #f4bdbd;
+			z-index: -2;
+		}
 	}
 </style>
-
