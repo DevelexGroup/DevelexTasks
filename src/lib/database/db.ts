1ab0f6900e549dd16539bf791aeacdb197add208
Michal Petr <jeerdus55@gmail.com>
2025-12-04 12:45:51 +0100

    feat(db): Simple db schema

---
+++ src/lib/database/db.ts
@@ -0,0 +1,14 @@
+﻿import Dexie, { type EntityTable } from 'dexie';
+import type { FixationDataEntry, GazeSampleDataEntry } from '$lib/database/db.types';
+
+const db = new Dexie("DevelexDatabase") as Dexie & {
+	gazeSamples: EntityTable<GazeSampleDataEntry, "id">
+	fixationData: EntityTable<FixationDataEntry, "id">
+};
+
+db.version(1).stores({
+	gazeSamples: '++id, child_id, session_id, task_name, timestamp, eyetracker_x, eyetracker_y, aoi, mouse_x, mouse_y, key_event, sound_name, error_type, task_result',
+	fixationData: '++id, child_id, session_id, task_name, timestamp, eyetracker_x, eyetracker_y, duration, aoi, fixation_index'
+});
+
+export { db };
---
+++ src/lib/database/db.types.ts
@@ -0,0 +1,29 @@
+﻿export interface GazeSampleDataEntry {
+	id?: number;
+	child_id: string;
+	session_id: string;
+	task_name: string;
+	timestamp: Date;
+	eyetracker_x: number | null;
+	eyetracker_y: number | null;
+	aoi: string[];
+	mouse_x: number;
+	mouse_y: number;
+	key_event: string[];
+	sound_name: string | null;
+	error_type: string[];
+	task_result: 'natural' | 'escape' | 'error'
+}
+
+export interface FixationDataEntry {
+	id?: number;
+	child_id: string;
+	session_id: string;
+	task_name: string;
+	timestamp: Date;
+	eyetracker_x: number | null;
+	eyetracker_y: number | null;
+	duration: number;
+	aoi: string[];
+	fixation_index: number;
+}
