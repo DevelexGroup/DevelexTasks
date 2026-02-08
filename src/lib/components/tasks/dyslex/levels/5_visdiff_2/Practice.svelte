5ca7f215d95e78df13d5052dfb35d6c04cea4abb
Javierko <javierkoo21@gmail.com>
2026-02-08 20:14:30 +0100

    chore(task/dyslex): remove second visdiff part

--- src/lib/components/tasks/dyslex/levels/5_visdiff_2/Practice.svelte
+++
@@ -1,5 +0,0 @@
-﻿<script lang="ts">
-	import { id } from '$lib/components/tasks/dyslex/levels/5_visdiff_2/index';
-</script>
-
-visdiff2 text
--- src/lib/components/tasks/dyslex/levels/5_visdiff_2/index.ts
+++
@@ -1,5 +0,0 @@
-import { resolveAny } from '$lib/utils/resolveAny';
-
-export const id = 'visdiff_2';
-
-export const instructionVideo = resolveAny('/video/cibule-instrukce-01.webm');
