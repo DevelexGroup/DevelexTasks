a84ca5f4bce7ca003c66f0fc11a1a9ccfca88e73
Michal Petr <jeerdus55@gmail.com>
2026-02-19 12:53:21 +0100

    chore: Fonologie data

--- src/lib/components/tasks/fonologie/fonologie.data.ts
+++ src/lib/components/tasks/fonologie/fonologie.data.ts
@@ -1,23 +1,353 @@
-﻿import type { TrackTaskData } from '$lib/types/task.types';
+﻿import type {
+	FonologieAudioRawDataEntry,
+	FonologieManipulationRawDataEntry
+} from '$lib/components/tasks/fonologie/fonologie.types';
 
-export const fonologieTestData: TrackTaskData = [
+export const fonologieL1Data: FonologieAudioRawDataEntry[] = [
 	{
-		levelID: 'level1',
-		label: 'Úroveň 1',
-		practiceContent: [
-			{
-				id: 'practice1',
-				sequence: ['delfín', 'kočka', 'dům', 'duch'],
-				correct: ['delfín', 'dům', 'duch'],
-				wordToRead: 'D'
-			}
-		],
-		content: [
-			{
-				id: 'content1',
-				sequence: [],
-				correct: ['apple']
-			}
-		]
+		task_id: 'fonologie',
+		id: 'FA2101',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level1',
+		sound: 'Ž_zac',
+		sequence: ['slepice2', 'zralok2', 'zelva2', 'zirafa2', 'slepice2'],
+		correct_indices: [1, 2, 3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2102',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level1',
+		sound: 'K_zac',
+		sequence: ['kure2', 'kocka2', 'lemur2', 'kralik2', 'kun2'],
+		correct_indices: [0, 1, 3, 4]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2103',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level1',
+		sound: 'M_zac',
+		sequence: ['motyl2', 'kocka2', 'mys2', 'meduza2', 'pes2'],
+		correct_indices: [0, 2, 3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2104',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level1',
+		sound: 'S_zac',
+		sequence: ['sova2', 'meduza2', 'slepice2', 'zelva2', 'zralok2'],
+		correct_indices: [0, 2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2105',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level1',
+		sound: 'P_zac',
+		sequence: ['papousek2', 'lemur2', 'kure2', 'sova2', 'pes2'],
+		correct_indices: [0, 4]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2106',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level1',
+		sound: 'L_zac',
+		sequence: ['lev2', 'lemur2', 'slepice2', 'zralok2', 'motyl2'],
+		correct_indices: [0, 1]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2201',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level2',
+		sound: 'K_kon',
+		sequence: ['kun2', 'zralok2', 'papousek2', 'kralik2', 'mys2'],
+		correct_indices: [1, 2, 3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2202',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level2',
+		sound: 'A_kon',
+		sequence: ['meduza2', 'kure2', 'sova2', 'kocka2', 'zralok2'],
+		correct_indices: [0, 2, 3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2203',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level2',
+		sound: 'A_kon',
+		sequence: ['zelva2', 'kosatka2', 'slepice2', 'kralik2', 'zirafa2'],
+		correct_indices: [0, 1, 4]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2204',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level2',
+		sound: 'E_kon',
+		sequence: ['slepice2', 'kure2', 'pes2', 'meduza2', 'zelva2'],
+		correct_indices: [0, 1]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2205',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level2',
+		sound: 'L_kon',
+		sequence: ['zelva2', 'zralok2', 'motyl2', 'buvol2', 'lev2'],
+		correct_indices: [2, 3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2206',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level2',
+		sound: 'S_kon',
+		sequence: ['sova2', 'pstros2', 'meduza2', 'kosatka2', 'pes2'],
+		correct_indices: [1, 4]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2301',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level3',
+		sound: 'O_upr',
+		sequence: ['papousek2', 'zirafa2', 'pstros2', 'pes2', 'sova2'],
+		correct_indices: [0, 2, 4]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2302',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level3',
+		sound: 'P_upr',
+		sequence: ['pstros2', 'slepice2', 'papousek1', 'lemur2', 'kosatka2'],
+		correct_indices: [1, 2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2303',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level3',
+		sound: 'V_upr',
+		sequence: ['lev2', 'kralik2', 'buvol2', 'zelva2', 'sova2'],
+		correct_indices: [2, 3, 4]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2304',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level3',
+		sound: 'L_upr',
+		sequence: ['rejnok2', 'kralik2', 'zelva2', 'zirafa2', 'zralok2'],
+		correct_indices: [1, 2, 4]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2305',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level3',
+		sound: 'A_upr',
+		sequence: ['kocka2', 'zralok2', 'zirafa2', 'kosatka2', 'buvol2'],
+		correct_indices: [1, 2, 3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2306',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level3',
+		sound: 'E_upr',
+		sequence: ['motyl2', 'lev2', 'mys2', 'pes2', 'buvol2'],
+		correct_indices: [1, 3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FA2307',
+		topic: 'zvířata',
+		set: 'animals2',
+		level: 'level3',
+		sound: 'T_upr',
+		sequence: ['motyl2', 'kosatka2', 'pstros2', 'meduza2', 'kralik2'],
+		correct_indices: [0, 1, 2]
+	}
+];
+
+const fonologieL4Data : FonologieManipulationRawDataEntry[] = [
+	{
+		task_id: 'fonologie',
+		id: 'FL4001',
+		level: 'level4',
+		sound: "4001",
+		model: ['vaza2'],
+		sequence: ['vana', 'vaha', 'duha', 'veka'],
+		correct_indices: [1]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4002',
+		level: 'level4',
+		sound: "4002",
+		model: ['krava'],
+		sequence: ['taska', 'hlava2', 'trava', 'triko'],
+		correct_indices: [2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4003',
+		level: 'level4',
+		sound: "4003",
+		model: ['vilaX'],
+		sequence: ['pirat1', 'pizza', 'vila', 'pila'],
+		correct_indices: [3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4004',
+		level: 'level4',
+		sound: "4004",
+		model: ['mesto'],
+		sequence: ['testo', 'triko', 'tenis', 'vesta'],
+		correct_indices: [0]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4005',
+		level: 'level4',
+		sound: "4005",
+		model: ['darek'],
+		sequence: ['porek', 'pisek', 'parek', 'oblek'],
+		correct_indices: [2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4006',
+		level: 'level4',
+		sound: "4006",
+		model: ['postel'],
+		sequence: ['kotel', 'kostel2', 'kost', 'pytel'],
+		correct_indices: [1]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4007',
+		level: 'level4',
+		sound: "4007",
+		model: ['mrak'],
+		sequence: ['drakX', 'dres', 'vrak', 'vlak'],
+		correct_indices: [0]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4008',
+		level: 'level4',
+		sound: "4008",
+		model: ['srot'],
+		sequence: ['hotel3', 'hory', 'hrot', 'hrob'],
+		correct_indices: [2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4009',
+		level: 'level4',
+		sound: "4009",
+		model: ['most'],
+		sequence: ['koste', 'kostel2', 'kotel', 'kost'],
+		correct_indices: [3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL4010',
+		level: 'level4',
+		sound: "4010",
+		model: ['brana2'],
+		sequence: ['vana', 'banan', 'vrana', 'farma'],
+		correct_indices: [2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL5001',
+		level: 'level5',
+		sound: "5001",
+		model: ['kocka3'],
+		sequence: ['kocar', 'hracka', 'volant', 'vlocka'],
+		correct_indices: [3]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL5003',
+		level: 'level5',
+		sound: "5003",
+		model: ['flaska'],
+		sequence: ['sasek', 'siska', 'hruska2', 'houska'],
+		correct_indices: [1]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL5004',
+		level: 'level5',
+		sound: "5004",
+		model: ['knizka'],
+		sequence: ['vazka3', 'tucnak5', 'tuzka', 'hruska2'],
+		correct_indices: [2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL5006',
+		level: 'level5',
+		sound: "5006",
+		model: ['rucnik'],
+		sequence: ['mostX', 'nocnik', 'slunecnik', 'cisnik'],
+		correct_indices: [1]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL5007',
+		level: 'level5',
+		sound: "5007",
+		model: ['jmeli'],
+		sequence: ['zima', 'zrni', 'zeli', 'zamek'],
+		correct_indices: [2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL5012',
+		level: 'level5',
+		sound: "5012",
+		model: ['panev'],
+		sequence: ['kokon', 'rakev', 'konev', 'vetev'],
+		correct_indices: [2]
+	},
+	{
+		task_id: 'fonologie',
+		id: 'FL5013',
+		level: 'level5',
+		sound: "5013",
+		model: ['pochod'],
+		sequence: ['obchod2', 'obal', 'oblek', 'vychod'],
+		correct_indices: [0]
 	}
 ];
---
+++ src/lib/components/tasks/fonologie/fonologie.types.ts
@@ -0,0 +1,18 @@
+﻿import type { RawDataEntry } from '$lib/types/data.types';
+
+export interface FonologieAudioRawDataEntry extends RawDataEntry {
+	topic: string;
+	set: string;
+	level: string;
+	sound: string;
+	sequence: string[];
+	correct_indices: number[];
+}
+
+export interface FonologieManipulationRawDataEntry extends RawDataEntry {
+	level: string;
+	sound: string;
+	model: string[];
+	sequence: string[];
+	correct_indices: number[];
+}
