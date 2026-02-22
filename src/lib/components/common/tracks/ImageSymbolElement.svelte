2b4ab6ee2b8d43e12236448a77f781923596562d
Javierko <javierkoo21@gmail.com>
2026-02-22 23:36:41 +0100

    fix(task): image symbol click background

--- src/lib/components/common/tracks/ImageSymbolElement.svelte
+++ src/lib/components/common/tracks/ImageSymbolElement.svelte
@@ -69,3 +69,84 @@
 		/>
 	</button>
 </GazeArea>
+
+<style>
+	.symbol {
+		cursor: pointer;
+		position: relative;
+		user-select: none;
+
+		&.non-interactable {
+			pointer-events: none;
+		}
+
+		&.selected {
+			&.correct-symbol {
+				color: #15803d;
+				&::before {
+					animation: correct-flash-bg 1000ms ease-in-out;
+				}
+			}
+
+			&.incorrect-symbol {
+				--animation-time: 300ms;
+				animation: incorrect-flash-text var(--animation-time) ease-in-out 2;
+				&::before {
+					animation: incorrect-flash-bg var(--animation-time) ease-in-out 2;
+				}
+			}
+		}
+
+		&::before {
+			content: '';
+			position: absolute;
+			inset: -0.375rem;
+			border-radius: 0.5rem;
+			background-color: rgba(0, 0, 0, 0);
+			transition: all 200ms ease-in;
+			pointer-events: none;
+			z-index: -2;
+		}
+
+		img {
+			position: relative;
+			z-index: 1;
+		}
+	}
+
+	@keyframes incorrect-flash-text {
+		0%,
+		100% {
+			color: inherit;
+		}
+
+		50% {
+			color: #dc2626;
+		}
+	}
+
+	@keyframes incorrect-flash-bg {
+		0%,
+		100% {
+			background-color: rgba(0, 0, 0, 0);
+			z-index: auto;
+		}
+
+		50% {
+			background-color: #f4bdbd;
+		}
+	}
+
+	@keyframes correct-flash-bg {
+		0%,
+		100% {
+			background-color: rgba(0, 0, 0, 0);
+			z-index: auto;
+		}
+
+		33%,
+		66% {
+			background-color: #bfe5cc;
+		}
+	}
+</style>
